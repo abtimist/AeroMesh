66dd3475100b2bcb5fbc41924cebbd5deafec6ae
Abtimist <vasagam.abhishek@gmail.com>
2026-09-26 17:30:45 +0530

    fix(ui): bust browser cache for map tiles

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -97,8 +97,8 @@ export default function MapView({ activeNode = 'India Node', alertPanelOpen, onA
 
   // Premium CartoDB Maps
   const tileUrl = dark 
-    ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
-    : "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png";
+    ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png?v=2"
+    : "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png?v=2";
 
   const currentConfig = NODE_CONFIG[activeNode] || NODE_CONFIG['India Node'];
 
